724919e08d064b3ef3a9b0c3c348a5d6324e8fcd
Matthias Probst <matthias.probst@kit.edu>
2022-08-17 16:23:56 +0200

    can specify ansys cfx inst dir via command line

--- h5rdmtoolbox/x2hdf/cfd/__init__.py
+++ h5rdmtoolbox/x2hdf/cfd/__init__.py
@@ -1,19 +1,18 @@
+import argparse
 import pathlib
 
 from .ansys.cfx import CFXCase
 
-import argparse
 
 def cfx2hdf(cfx_filename: pathlib.Path):
     cfx_case = CFXCase(cfx_filename)
     return cfx_case.hdf.generate(True)
 
 
-
-
 def _cfx2hdf():
     """Command line interface method. A folder is expected"""
     import pathlib
+    from .ansys import AnsysInstallation
     parser = argparse.ArgumentParser(description='PIV uncertainty estimation with CNN')
 
     # Add the arguments
@@ -21,9 +20,23 @@ def _cfx2hdf():
                         type=str,
                         required=False,
                         help='CFX file (*.cfx)')
+    parser.add_argument('-inst', '--installation_directory',
+                        type=str,
+                        required=False,
+                        default=None,
+                        help='Path to installation directory of ansys cfx')
 
     args = parser.parse_args()
 
-    cfx_filename = pathlib.Path(args.file)
-    hdf_filename = cfx2hdf(cfx_filename)
-    print(f'Generated {hdf_filename} from {cfx_filename}')
+    ansys_inst = AnsysInstallation()
+    if args.installation_directory is not None:
+        ansys_inst.installation_directory = args.installation_directory
+
+    installation_directory = ansys_inst.installation_directory
+    if installation_directory is None:
+        raise FileNotFoundError('Ansys installation directory is unknown. Run cfx2hdf -inst INST_DIR')
+
+    if args.file is not None:
+        cfx_filename = pathlib.Path(args.file)
+        hdf_filename = cfx2hdf(cfx_filename)
+        print(f'Generated {hdf_filename} from {cfx_filename}')
