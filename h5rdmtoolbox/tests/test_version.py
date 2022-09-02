f094f046baead3e54b5cae1f56908e4a197dd750
Probst <matthias.probst@kit.edu>
2022-09-02 16:24:35 +0200

    correct version

--- h5rdmtoolbox/tests/test_version.py
+++ h5rdmtoolbox/tests/test_version.py
@@ -5,4 +5,4 @@ import h5rdmtoolbox as h5tbx
 
 class TestVersion(unittest.TestCase):
     def test_version(self):
-        self.assertEqual(h5tbx.__version__, '0.1.12')
+        self.assertEqual(h5tbx.__version__, '0.1.11')
