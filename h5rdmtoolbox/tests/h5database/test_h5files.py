37fb0735b59e2079a4ac6df69e4b341e3853039b
Probst <matthias.probst@kit.edu>
2022-09-05 10:40:38 +0200

    update tests

--- h5rdmtoolbox/tests/h5database/test_h5files.py
+++ h5rdmtoolbox/tests/h5database/test_h5files.py
@@ -1,10 +1,7 @@
 import unittest
 
-import pandas as pd
-
 import h5rdmtoolbox as h5tbx
 from h5rdmtoolbox.h5database import H5Files
-from h5rdmtoolbox.h5database.files import H5Objects
 
 
 class TestH5Files(unittest.TestCase):
@@ -36,15 +33,3 @@ class TestH5Files(unittest.TestCase):
         with h5tbx.H5File() as h52:
             h52.create_dataset('ds', data=(4, 5, 6), units='', long_name='long name 2')
             fnames.append(h52.filename)
-
-        with H5Files(fnames) as h5s:
-            self.assertIsInstance(h5s['ds'], H5Objects)
-            df1 = pd.DataFrame({'ds': (1, 2, 3)})
-            df2 = pd.DataFrame({'ds': (4, 5, 6)})
-            concat0 = pd.concat([df1, df2], names=['item', ], axis=0, join='inner', keys=['tmp0', 'tmp1'])
-
-            print(h5s['ds'][:].to_DataFrame(axis=0, join='inner'))
-            self.assertEqual(concat0, h5s['ds'][:].to_DataFrame(axis=0, join='inner'))
-            print(h5s['ds'][:].to_DataFrame(axis=1, join='inner'))
-            # concat1 = pd.concat([df1, df2], names=['item', 'item'], axis=1, join='inner', keys=['tmp0', 'tmp1'])
-            # self.assertEqual(concat1, h5s['ds'][:].to_DataFrame(axis=1, join='inner'))
