34f9d562024929eb77435989503d90cb00a05c17
MP <matth.probst@gmail.com>
2022-11-25 12:21:25 +0100

    plotting update

--- h5rdmtoolbox/plotting.py
+++ h5rdmtoolbox/plotting.py
@@ -5,8 +5,23 @@ import matplotlib.pyplot as plt
 from .config import CONFIG as config
 
 
+def build_label_unit_str(name: str, units: str,
+                         units_format: str = config.XARRAY_UNIT_REPR_IN_PLOTS) -> str:
+    """generates the label string from a name and a units based on the units format"""
+    units = units.replace("**", "^")
+    if units_format == 'in':
+        return f'{name} in ${units}$'
+    elif units_format == '/':
+        return f'{name} / ${units}$'
+    elif units_format in ('[', ']', '[]'):
+        return f'{name} [${units}$]'
+    elif units_format in ('(', ')', '()'):
+        return f'{name} (${units}$)'
+
+
 class XarrayLabelManipulation(plt.Axes):
-    def _adjust_units_label(self, label, units_format=config.XARRAY_UNIT_REPR_IN_PLOTS):
+    @staticmethod
+    def _adjust_units_label(label, units_format=config.XARRAY_UNIT_REPR_IN_PLOTS):
         # other formats: '(', '[', '/', 'in'
         if label:
             if not label[-1] == ']':
@@ -15,17 +30,23 @@ class XarrayLabelManipulation(plt.Axes):
                 return label
             idx0 = label.rfind('[', 1)
             units_string = label[idx0:]
+            _raw_unit = f"${units_string[1:-1].replace('**', '^')}$"
+
             if units_format == 'in':
-                return label.replace(units_string, f' in {units_string[1:-1]}')
+                return label.replace(units_string, f' in {_raw_unit}')
             if units_format == '/':
-                return label.replace(units_string, f' / {units_string[1:-1]}')
-            if units_format == '(in)':
-                return label.replace(units_string, f' ({units_string[1:-1]})')
+                return label.replace(units_string, f' / {_raw_unit}')
+            if units_format in ('(', ')', '()'):
+                return label.replace(units_string, f' ({_raw_unit})')
+            else:
+                return label
 
     def set_xlabel(self, xlabel, *args, **kwargs):
+        """set the (adjusted) xlabel"""
         super().set_xlabel(self._adjust_units_label(xlabel), *args, **kwargs)
 
     def set_ylabel(self, ylabel, *args, **kwargs):
+        """set the (adjusted) ylabel"""
         super().set_ylabel(self._adjust_units_label(ylabel), *args, **kwargs)
 
 
---
+++ h5rdmtoolbox/xr_accessors/scatterhist.py
@@ -0,0 +1,115 @@
+"""scatter hist accessor
+
+Plots scatter plot with histograms at sides similar to
+https://matplotlib.org/3.1.0/gallery/lines_bars_and_markers/scatter_hist.html
+where most code is taken from
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
+
+from ..plotting import build_label_unit_str
+
+
+@xr.register_dataarray_accessor("scatterhist")
+class ScatterHist:
+    """Scatter-histogram-plot for xr.DataArrays
+
+    Example:
+    --------
+    da = xr.DataArray( ... )
+    da[:].scatterhist(marker='+', color='k')
+    """
+
+    def __init__(self, xarray_obj):
+        self._obj = xarray_obj
+
+    def __call__(self, *args, **kwargs):
+        self.plot(*args, **kwargs)
+
+    def plot(self, **kwargs):
+        """Plot a scatter histogram.
+        Code taken from https://matplotlib.org/3.1.0/gallery/lines_bars_and_markers/scatter_hist.html"""
+        ds_x = self._obj[list(self._obj.coords.variables.keys())[kwargs.get('axis', 0)]]
+        ds_y = self._obj
+        x = ds_x.values
+        y = ds_y.data
+
+        # definitions for the axes
+        left, width = 0.1, 0.65
+        bottom, height = 0.1, 0.65
+        spacing = 0.005
+
+        rect_scatter = [left, bottom, width, height]
+        rect_histx = [left, bottom + height + spacing, width, 0.2]
+        rect_histy = [left + width + spacing, bottom, 0.2, height]
+
+        # start with a rectangular Figure
+        fig = kwargs.get('fig', plt.figure(figsize=kwargs.get('figsize', (8, 8))))
+
+        ax_scatter = plt.axes(rect_scatter)
+        ax_scatter.tick_params(direction='in', top=True, right=True)
+        ax_histx = plt.axes(rect_histx)
+        ax_histx.tick_params(direction='in', labelbottom=False)
+        ax_histy = plt.axes(rect_histy)
+        ax_histy.tick_params(direction='in', labelleft=False)
+
+        # the scatter plot:
+        ax_scatter.scatter(x, y,
+                           marker=kwargs.pop('marker', None),
+                           s=kwargs.pop('s', None),
+                           color=kwargs.get('color', 'k'))
+
+        # now determine nice limits by hand:
+        bins = kwargs.get('bins', 10)
+        xmean = x.mean()
+        dx = x.max() - x.min()
+        xbinwidth = dx / bins
+        _xlim = (xmean - dx / 2, xmean + dx / 2)
+        ymean = y.mean()
+        dy = y.max() - y.min()
+        ybinwidth = dy / bins
+        _ylim = (ymean - dy / 2, ymean + dy / 2)
+        # xlim = np.ceil(np.abs(x).max() / binwidth) * binwidth
+        # ylim = np.ceil(np.abs(y).max() / binwidth) * binwidth
+        ax_scatter.set_xlim(kwargs.get('xlim', _xlim))
+        ax_scatter.set_ylim(kwargs.get('ylim', _ylim))
+
+        xunits = ds_x.attrs.get('units', None)
+        if 'standard_name' in ds_x.attrs:
+            xname = ds_x.standard_name
+        elif 'long_name' in ds_x.attrs:
+            xname = ds_x.long_name
+            ax_scatter.set_xlabel(ds_x.long_name)
+        else:
+            xname = ds_x.name
+        if xunits:
+            ax_scatter.set_xlabel(build_label_unit_str(xname, xunits))
+        else:
+            ax_scatter.set_xlabel(xname)
+
+        yunits = ds_y.attrs.get('units', None)
+        if 'standard_name' in ds_y.attrs:
+            yname = ds_y.standard_name
+        elif 'long_name' in ds_y.attrs:
+            yname = ds_y.long_name
+            ax_scatter.set_ylabel(ds_y.long_name)
+        else:
+            yname = ds_y.name
+        if yunits:
+            ax_scatter.set_ylabel(build_label_unit_str(yname, yunits))
+        else:
+            ax_scatter.set_ylabel(yname)
+
+        xbins = np.arange(_xlim[0], _xlim[1], xbinwidth)
+        ybins = np.arange(_ylim[0], _ylim[1], ybinwidth)
+        ax_histx.hist(x, bins=kwargs.get('xbins', xbins),
+                      color=kwargs.get('color', 'k'))
+        ax_histy.hist(y, bins=kwargs.get('ybins', ybins), orientation='horizontal',
+                      color=kwargs.get('color', 'k'))
+
+        ax_histx.set_xlim(ax_scatter.get_xlim())
+        ax_histy.set_ylim(ax_scatter.get_ylim())
+
+        return fig
