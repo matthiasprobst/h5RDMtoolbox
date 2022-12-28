df31b0f2dea9bf849cd3ea7d81fe21b7808f1458
MP <matth.probst@gmail.com>
2022-12-28 15:34:38 +0100

    add test for find

---
+++ tests/wrapper/test_find.py
@@ -0,0 +1,33 @@
+import logging
+import unittest
+from pint_xarray import unit_registry as ureg
+
+import h5rdmtoolbox as h5tbx
+from h5rdmtoolbox import config
+from h5rdmtoolbox import use
+from h5rdmtoolbox.wrapper import set_loglevel
+
+use('cflike')
+logger = logging.getLogger('h5rdmtoolbox.wrapper')
+set_loglevel('ERROR')
+
+ureg.default_format = config.CONFIG.UREG_FORMAT
+
+
+class TestFind(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """setup"""
+        with h5tbx.H5File() as h5:
+            h5.attrs['project'] = 'tutorial'
+            h5.create_dataset('velocity', data=[1, 2, -1], units='m/s', standard_name='x_velocity')
+            g = h5.create_group('sub')
+            g.create_dataset('velocity', data=[4, 0, -3, 12, 3], units='m/s', standard_name='x_velocity')
+            h5.dump()
+            self.filename = h5.hdf_filename
+
+    def test_find_rec_False(self):
+        with h5tbx.H5File(self.filename) as h5:
+            print('\nfind basename=velocity in sub/:')
+            self.assertEqual(h5['sub'].find({'$basename': 'velocity'}, '$dataset', rec=False)[0],
+                             h5['sub/velocity'])
