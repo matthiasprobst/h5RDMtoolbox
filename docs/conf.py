3c27e4fc37328a3eadd187ab8c6b10575dac0015
Matthias Probst <matth.probst@gmail.com>
2023-03-01 19:31:29 +0100

    resolved merge conflicts

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,7 @@ author = 'Matthias Probst'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.navigationtree',
-              'sphinx.ext.autodoc',
+extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.napoleon',
               'sphinx.ext.viewcode',
               "sphinx.ext.extlinks",
