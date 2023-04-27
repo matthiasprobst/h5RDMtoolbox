fc84ae70d8d0247cb34fcb12cc23160ec363d2c7
MP <matth.probst@gmail.com>
2023-04-27 08:49:37 +0200

    bugfix unit testing returns always bool

--- h5rdmtoolbox/conventions/standard_name.py
+++ h5rdmtoolbox/conventions/standard_name.py
@@ -11,23 +11,22 @@ Examples for naming tables:
     - standard name table (http://cfconventions.org/Data/cf-standard-names/current/build/cf-standard-name-table.html)
     - CGNS data name convention (https://cgns.github.io/CGNS_docs_current/sids/dataname.html)
 """
+import h5py
 import json
 import os
+import pandas as pd
 import pathlib
 import re
 import warnings
 import xml.etree.ElementTree as ET
-from datetime import datetime
-from enum import Enum
-from pathlib import Path
-from typing import Dict, Union, List, Tuple
-
-import h5py
-import pandas as pd
 import yaml
 from IPython.display import display, HTML
+from datetime import datetime
+from enum import Enum
 from omegaconf import DictConfig, OmegaConf
+from pathlib import Path
 from pint.errors import UndefinedUnitError
+from typing import Dict, Union, List, Tuple
 
 from ._logger import logger
 from .standard_attribute import StandardAttribute
@@ -72,9 +71,10 @@ def verify_unit_object(_units) -> bool:
     """Raise error if _units is not processable by pint package. Otherwise return True"""
     try:
         ureg.Unit(_units)
+        return True
     except UndefinedUnitError as e:
-        raise UndefinedUnitError(f'Units cannot be understood using ureg package: {_units}. --> {e}')
-    return True
+        logger.error(UndefinedUnitError(f'Units cannot be understood using ureg package: {_units}. --> {e}'))
+        return False
 
 
 def xmlsnt2dict(xml_filename: Path) -> Tuple[dict, dict]:
@@ -284,7 +284,8 @@ class MinimalStandardNameTable:
         if name in self.table:
             raise StandardNameError(f'name "{name}" already exists in table. Use modify() '
                                     'to change the content')
-        verify_unit_object(canonical_units)
+        if not verify_unit_object(canonical_units):
+            raise ValueError(f'Canonical units "{canonical_units}" is not a valid unit object')
         self._table[name] = dict(description=description, canonical_units=canonical_units)
 
     def modify(self, name: str, description: str, canonical_units: str):
