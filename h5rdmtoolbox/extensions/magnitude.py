bugfix magnitude computation from vector ext

h5rdmtoolbox/extensions/magnitude.py
@@ -38,7 +38,7 @@ class MagnitudeAccessor:
         """
         mag2 = self._obj[data_vars[0]].pint.quantify() ** 2
         for data_var in data_vars[1:]:
-            self._obj[data_var].pint.quantify() ** 2
+            mag2 += self._obj[data_var].pint.quantify() ** 2
         with xr.set_options(keep_attrs=True):
             mag = np.sqrt(mag2).pint.dequantify()
 

tests/test_extensions.py
@@ -1,3 +1,4 @@
+import numpy as np
 import unittest
 import xarray as xr
 
@@ -10,6 +11,18 @@ class TestExtension(unittest.TestCase):
 
     def setUp(self) -> None:
         h5tbx.use(None)
+
+    def test_HDFXrDataset(self):
+        with h5tbx.File() as h5:
+            u = h5.create_dataset('u', data=np.arange(10, 20))
+            v = h5.create_dataset('v', data=np.arange(10, 100))
+            v2 = h5.create_dataset('v2', data=np.arange(10, 20))
+            with self.assertRaises(ValueError):
+                vector.HDFXrDataset(u=u, v=v)
+            vel = vector.HDFXrDataset(u=u, v=v2)
+            self.assertEqual(vel.data_vars, ['u', 'v'])
+            self.assertEqual(vel.shape, (10,))
+            self.assertIsInstance(vel[:], xr.Dataset)
 
     def test_Vector(self):
         """Test the Vector special dataset"""
@@ -34,12 +47,17 @@ class TestExtension(unittest.TestCase):
                 h5.Vector(u='u', v=6.5)
             vec.magnitude.compute_from('uu', 'vv', inplace=True)
             self.assertTrue('magnitude_of_uu_vv' in vec.data_vars)
+            for i in range(3):
+                self.assertEqual(float(vec.magnitude_of_uu_vv[i].values),
+                                 np.sqrt(float(vec.uu[i].values) ** 2 + float(vec.vv[i].values) ** 2))
+
             vec.magnitude.compute_from('uu', 'vv', name='speed', inplace=True)
             self.assertTrue('speed' in vec.data_vars)
             with self.assertRaises(KeyError):
                 vec.magnitude.compute_from('uu', 'vv', name='speed', inplace=True)
             vec.magnitude.compute_from('uu', 'vv', name='speed2', inplace=True, overwrite=True, attrs={'test': 1})
             self.assertEqual(vec.speed2.attrs['test'], 1)
-            vec2 = vec.magnitude.compute_from('uu', 'vv', name='speed2', inplace=False, overwrite=True, attrs={'test': 1})
+            vec2 = vec.magnitude.compute_from('uu', 'vv', name='speed2', inplace=False, overwrite=True,
+                                              attrs={'test': 1})
             self.assertEqual(vec2.attrs['test'], 1)
             self.assertIsInstance(vec2, xr.DataArray)