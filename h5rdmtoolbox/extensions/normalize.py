67449daaaa91b7673268fccb24b1f8913c2351c5
Matthias Probst <matth.probst@gmail.com>
2023-06-12 08:15:25 +0200

    bugfixes units

--- h5rdmtoolbox/extensions/normalize.py
+++ h5rdmtoolbox/extensions/normalize.py
@@ -1,6 +1,15 @@
 """Extension to compute normalized xarray datasets and arrays"""
+import pint
 # import pint_xarray  # noqa: F401
 import xarray as xr
+
+
+def to_base_units(da: xr.DataArray) -> xr.DataArray:
+    """Turns the units of an xarray to the base units, e.g. m/mm turns to dimensionless
+    because pint_xarray has no method `to_base_units()`.
+    """
+    final_units = pint.Quantity(da.attrs['units']).to_base_units()
+    return da.pint.quantify().pint.to(final_units.units).pint.dequantify()
 
 
 @xr.register_dataarray_accessor("normalize")
@@ -15,6 +24,28 @@ class NormalizeAccessor:
 
     def __call__(self, data_normalization, **coords):
         """normalizes the data, not the coords unless the coords are specified"""
+        if len(coords):
+            new_obj = self._obj.normalize.coords(**coords)
+        else:
+            new_obj = self._obj
+
+        if 'units' not in self._obj.attrs:
+            self._obj.attrs['units'] = ''
+
+        if isinstance(data_normalization, str):
+            magnitude, units = data_normalization.split(' ')
+            if '.' in magnitude or 'e' in magnitude:
+                data_normalization = xr.DataArray(float(data_normalization), attrs={'units': units})
+            else:
+                data_normalization = xr.DataArray(int(data_normalization), attrs={'units': units})
+        elif isinstance(data_normalization, (int, float)):
+            data_normalization = xr.DataArray(data_normalization, attrs={'units': self._obj.attrs['units']})
+
+        if self._obj.attrs['units'] not in ('', ' ', 'dimensionless'):
+            return (new_obj.pint.quantify() / data_normalization.pint.quantify().pint.to(
+                self._obj.attrs['units'])).pint.dequantify()
+        else:
+            return (new_obj.pint.quantify() / data_normalization.pint.quantify()).pint.dequantify()
 
     def coords(self, *value, **coords):
         """Normalize data
@@ -34,33 +65,62 @@ class NormalizeAccessor:
             raise ValueError('Only one value can be specified.')
         elif len(value) == 1:
             coords = {k: value[0] for k in self._obj.coords.keys()}
-        for k in coords:
-            if k not in self._obj.coords:
-                raise KeyError(f'Coordinate "{k}" is not in the dataset.')
+
+        if len(value) == 0:
+            if len(coords) not in (1, len(self._obj.coords)):
+                raise ValueError(f'Either all coordinates must be specified or only one coordinate: {len(coords)}!= (1,'
+                                 f' {len(self._obj.coords)}).')
+
+        if not isinstance(coords, dict):
+            raise TypeError(f'coords must be a dictionary, not {type(coords)}.')
 
         for k in self._obj.coords.keys():
             if 'units' not in self._obj.coords[k].attrs:
                 self._obj.coords[k].attrs['units'] = ''
 
-        new_coords = {}
-        for k in coords:
-            if isinstance(coords[k], (float, int)):
-                new_coords[k] = xr.DataArray(coords[k], attrs={'units': self._obj.coords[k].attrs['units']})
-            elif isinstance(coords[k], xr.DataArray):
-                new_coords[k] = coords[k]
+        obj_coords = {k: xr.DataArray(v.values, dims=v.dims, attrs=v.attrs) for k, v in self._obj.coords.items()}
+
+        if len(coords) == 1:
+            # either the key is different to the coords, then this is used to normalize all coordinates
+            # or the key is the same as the coords, then this is used to normalize only this coordinate
+            coord_key = list(coords.keys())[0]
+            coord_value = coords[coord_key]
+
+            if isinstance(coord_value, (int, float)):
+                coords = {coord_key: xr.DataArray(coord_value, attrs={'units': ''}) for k in obj_coords.keys()}
+            elif isinstance(coord_value, str):
+                quantity = pint.Quantity(coord_value)
+                magnitude, units = quantity.magnitude, quantity.units
+                if '.' in magnitude or 'e' in magnitude:
+                    coords = {coord_key: xr.DataArray(float(magnitude), attrs={'units': units}) for k in
+                              obj_coords.keys()}
+                else:
+                    coords = {coord_key: xr.DataArray(int(magnitude), attrs={'units': units}) for k in
+                              obj_coords.keys()}
+            elif isinstance(coord_value, xr.DataArray):
+                coords = {coord_key: coord_value for k in obj_coords.keys()}
             else:
-                raise TypeError(f'Coordinate "{k}" is of type {type(coords[k])}. '
+                raise TypeError(f'Coordinate "{coord_key}" is of type {type(coord_value)}. '
                                 f'It must be either a float or an xarray data array.')
 
-        # hack as coordinates cannot be "pinted" (https://docs.xarray.dev/en/stable/generated/xarray.DataArray.expand_dims.html):
-        old_coords = {k: xr.DataArray(v.values, attrs=v.attrs.items(), dims=v.dims) for k, v in
-                      self._obj.coords.items()}
+            if coord_key not in obj_coords.keys():
+                updated_coords = {k: (v.pint.quantify() / coords[coord_key].pint.quantify()).pint.dequantify() for k, v
+                                  in obj_coords.items()}
 
-        new_coords = {k: (old_coords[k].pint.quantify() / v.pint.quantify().pint.to(
-            self._obj.coords[k].attrs['units'])).pint.dequantify() for k, v in
-                      new_coords.items()}
+                ret_obj = self._obj.assign_coords(**updated_coords)
+                for k in ret_obj.coords.keys():
+                    ret_obj = ret_obj.rename({k: f'{k} / {coord_key}'})
+                return ret_obj
+            else:
+                # update only specific one:
+                new_coord = to_base_units((obj_coords[coord_key].pint.quantify() / coords[
+                    coord_key].pint.quantify()).pint.dequantify())
+                return self._obj.assign_coords(**{coord_key: new_coord})
 
-        # for k in new_coords.keys():
-        #     new_coords[k].dims = self._obj.coords[k].dims
-        ret_obj = self._obj.assign_coords(**new_coords)
+        if not all(k in obj_coords.keys() for k in coords.keys()):
+            raise ValueError('All normalization items must be coordinates if more than one item is provided.')
+
+        ret_obj = self._obj
+        for k, v in coords.items():
+            ret_obj = ret_obj.normalize.coords(**{k: v})
         return ret_obj
--- tests/test_extensions.py
+++ tests/test_extensions.py
@@ -4,7 +4,7 @@ import xarray as xr
 
 import h5rdmtoolbox as h5tbx
 # noinspection PyUnresolvedReferences
-from h5rdmtoolbox.extensions import vector, magnitude
+from h5rdmtoolbox.extensions import normalize, vector, magnitude
 
 
 class TestExtension(unittest.TestCase):
@@ -62,10 +62,94 @@ class TestExtension(unittest.TestCase):
             self.assertEqual(vec2.attrs['test'], 1)
             self.assertIsInstance(vec2, xr.DataArray)
 
-    def test_norm(self):
-        # noinspection PyUnresolvedReferences
-        from h5rdmtoolbox.extensions import normalize
+    def test_norm_one_for_all_with_unit(self):
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True)
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True)
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords(L='3 m')
+            self.assertIn('x / L', newu.dims)
+            self.assertIn('y / L', newu.dims)
+            self.assertIn('x / L', newu.coords)
+            self.assertIn('y / L', newu.coords)
+            self.assertEqual(newu['x / L'].attrs['units'], '1 / meter')
 
+    def test_norm_one_for_all_without_unit(self):
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True)
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True)
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords(L='3')
+            self.assertIn('x / L', newu.dims)
+            self.assertIn('y / L', newu.dims)
+            self.assertIn('x / L', newu.coords)
+            self.assertIn('y / L', newu.coords)
+            self.assertEqual(newu['x / L'].attrs['units'], 'dimensionless')
+
+    def test_norm_only_one_without_unit(self):
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True)
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True)
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords(x='3')
+            self.assertIn('x', newu.dims)
+            self.assertIn('y', newu.dims)
+            self.assertIn('x', newu.coords)
+            self.assertIn('y', newu.coords)
+            self.assertEqual(newu['x'][0].values, 1 / 3)
+            self.assertEqual(newu['x'][1].values, 2 / 3)
+            self.assertEqual(newu['x'][2].values, 3 / 3)
+            self.assertEqual(newu['y'][0].values, 1)
+            self.assertEqual(newu['y'][1].values, 2)
+            self.assertEqual(newu['y'][2].values, 3)
+            self.assertEqual(newu['x'].attrs['units'], 'dimensionless')
+            self.assertEqual(newu['y'].attrs['units'], '')
+
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True)
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True)
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords(y='3')
+            self.assertIn('x', newu.dims)
+            self.assertIn('y', newu.dims)
+            self.assertIn('x', newu.coords)
+            self.assertIn('y', newu.coords)
+            self.assertEqual(newu['y'][0].values, 1 / 3)
+            self.assertEqual(newu['y'][1].values, 2 / 3)
+            self.assertEqual(newu['y'][2].values, 3 / 3)
+            self.assertEqual(newu['y'][3].values, 4 / 3)
+            self.assertEqual(newu['x'][0].values, 1)
+            self.assertEqual(newu['x'][1].values, 2)
+            self.assertEqual(newu['x'][2].values, 3)
+            self.assertEqual(newu['y'].attrs['units'], 'dimensionless')
+            self.assertEqual(newu['x'].attrs['units'], '')
+
+    def test_norm_coords_without_unit(self):
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True)
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True)
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords(y='3', x='4')
+            self.assertIn('x', newu.dims)
+            self.assertIn('y', newu.dims)
+            self.assertIn('x', newu.coords)
+            self.assertIn('y', newu.coords)
+            self.assertEqual(newu['y'][0].values, 1 / 3)
+            self.assertEqual(newu['y'][1].values, 2 / 3)
+            self.assertEqual(newu['y'][2].values, 3 / 3)
+            self.assertEqual(newu['y'][3].values, 4 / 3)
+            self.assertEqual(newu['x'][0].values, 1 / 4)
+            self.assertEqual(newu['x'][1].values, 2 / 4)
+            self.assertEqual(newu['x'][2].values, 3 / 4)
+            self.assertEqual(newu['y'].attrs['units'], 'dimensionless')
+            self.assertEqual(newu['x'].attrs['units'], 'dimensionless')
+
+    def test_norm(self):
         with h5tbx.File() as h5:
             h5.create_dataset('x', data=[1, 2, 3], make_scale=True)
             h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True)
@@ -88,7 +172,7 @@ class TestExtension(unittest.TestCase):
             self.assertIn('y', newu.dims)
             self.assertIn('x', newu.coords)
             self.assertIn('y', newu.coords)
-            self.assertEqual(newu.x.attrs['units'], 'dimensionless')
+            self.assertEqual(newu.x.attrs['units'], 'meter')
             self.assertEqual(newu.x[0].values, 1 / 3)
             self.assertEqual(newu.x[1].values, 2 / 3)
             self.assertEqual(newu.x[2].values, 3 / 3)
@@ -107,7 +191,7 @@ class TestExtension(unittest.TestCase):
             self.assertIn('y', newu.dims)
             self.assertIn('x', newu.coords)
             self.assertIn('y', newu.coords)
-            self.assertEqual(newu.x.attrs['units'], 'dimensionless')
+            self.assertEqual(newu.x.attrs['units'], 'meter')
             self.assertEqual(newu.x[0].values, 1 / 3)
             self.assertEqual(newu.x[1].values, 2 / 3)
             self.assertEqual(newu.x[2].values, 3 / 3)
@@ -116,6 +200,7 @@ class TestExtension(unittest.TestCase):
             self.assertEqual(newu.y[2].values, 3)
             self.assertEqual(newu.y[3].values, 4)
 
+    def test_norm2(self):
         with h5tbx.File() as h5:
             h5.create_dataset('x', data=[1, 2, 3], make_scale=True, attrs={'units': 'm'})
             h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True, attrs={'units': 'm'})
@@ -145,6 +230,25 @@ class TestExtension(unittest.TestCase):
             self.assertIn('y', newu.dims)
             self.assertIn('x', newu.coords)
             self.assertIn('y', newu.coords)
+            self.assertEqual(newu.x.attrs['units'], 'meter')
+            self.assertEqual(newu.x[0].values, 1 / 3)
+            self.assertEqual(newu.x[1].values, 2 / 3)
+            self.assertEqual(newu.x[2].values, 3 / 3)
+            self.assertEqual(newu.y[0].values, 1 / 3)
+            self.assertEqual(newu.y[1].values, 2 / 3)
+            self.assertEqual(newu.y[2].values, 3 / 3)
+            self.assertEqual(newu.y[3].values, 4 / 3)
+
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True, attrs={'units': 'm'})
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True, attrs={'units': 'm'})
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords('3 m')
+            self.assertIn('x', newu.dims)
+            self.assertIn('y', newu.dims)
+            self.assertIn('x', newu.coords)
+            self.assertIn('y', newu.coords)
             self.assertEqual(newu.x.attrs['units'], 'dimensionless')
             self.assertEqual(newu.x[0].values, 1 / 3)
             self.assertEqual(newu.x[1].values, 2 / 3)
@@ -153,3 +257,22 @@ class TestExtension(unittest.TestCase):
             self.assertEqual(newu.y[1].values, 2 / 3)
             self.assertEqual(newu.y[2].values, 3 / 3)
             self.assertEqual(newu.y[3].values, 4 / 3)
+
+        with h5tbx.File() as h5:
+            h5.create_dataset('x', data=[1, 2, 3], make_scale=True, attrs={'units': 'm'})
+            h5.create_dataset('y', data=[1, 2, 3, 4], make_scale=True, attrs={'units': 'm'})
+            h5.create_dataset('u', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            h5.create_dataset('v', data=np.random.random((4, 3)), attach_scales=('y', 'x'))
+            newu = h5.u[:].normalize.coords('3 mm')
+            self.assertIn('x', newu.dims)
+            self.assertIn('y', newu.dims)
+            self.assertIn('x', newu.coords)
+            self.assertIn('y', newu.coords)
+            self.assertEqual(newu.x.attrs['units'], 'dimensionless')
+            self.assertEqual(newu.x[0].values, 1 / 3 * 1000)
+            self.assertEqual(newu.x[1].values, 2 / 3 * 1000)
+            self.assertEqual(newu.x[2].values, 3 / 3 * 1000)
+            self.assertEqual(newu.y[0].values, 1 / 3 * 1000)
+            self.assertEqual(newu.y[1].values, 2 / 3 * 1000)
+            self.assertEqual(newu.y[2].values, 3 / 3 * 1000)
+            self.assertEqual(newu.y[3].values, 4 / 3 * 1000)
