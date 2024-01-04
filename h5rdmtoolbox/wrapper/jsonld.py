1f5048e5e618e5bf1bcc0dc28ede87637a512aaf
Matthias Probst <matth.probst@gmail.com>
2024-01-04 13:49:49 +0100

    remove print statement [skip ci]

--- h5rdmtoolbox/wrapper/jsonld.py
+++ h5rdmtoolbox/wrapper/jsonld.py
@@ -60,7 +60,6 @@ def dumpd(grp,
         for k in node.attrs.keys():
             if not k.isupper():
                 v = node.attrs[k]
-                print(k, v)
                 if node.iri.predicate.get(k, None) is not None:
                     if node.iri.object.get(k, None) is not None:
                         value = str(node.iri.object[k])
