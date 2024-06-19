bc87bdb9035cc1d356fb40eca114f2a9fddb881f
Matthias Probst <matth.probst@gmail.com>
2024-06-19 14:11:35 +0200

    bugfix

--- tests/test_xarray_export.py
+++ tests/test_xarray_export.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 import unittest
 
@@ -24,14 +25,22 @@ class TestXrExport(unittest.TestCase):
         self.assertIsInstance(da2, xr.DataArray)
         self.assertEqual(da2[()], da[()])
 
-    def test_export_timeda(self):
+    def test_export_timedata(self):
         with h5tbx.File() as h5:
             ds = h5.create_time_dataset('ds', data=datetime.now(),
-                                        format='iso')
+                                        time_format='iso')
             da = ds[()]
 
         self.assertIsInstance(da, xr.DataArray)
         nc_fname = h5tbx.utils.generate_temporary_filename(suffix='.nc')
+
+        for ak in da.attrs.keys():
+            if isinstance(da.attrs[ak], dict):
+                da.attrs[ak] = json.dumps(da.attrs[ak])
+
+        # TODO: DONT use "@" in xarray! conflict with to_netcdf()
+        da.attrs['RDF_TYPE'] = da.attrs.pop('@TYPE')
+
         da.to_netcdf(nc_fname)
         da2 = xr.open_dataarray(nc_fname)
         self.assertIsInstance(da2, xr.DataArray)
