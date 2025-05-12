52200cbfeab252fc84a673d46196e51dcfb1077a
MatthiasProbst <matth.probst@gmail.com>
2025-05-12 21:14:59 +0200

    remove standard name implementations

--- tests/conventions/test_utils.py
+++ tests/conventions/test_utils.py
@@ -1,11 +1,11 @@
 import json
 import pathlib
-import pint
 import unittest
+
+import pint
 import yaml
 
 from h5rdmtoolbox.convention import utils
-from h5rdmtoolbox.convention.standard_names.table import StandardNameTable
 from h5rdmtoolbox.utils import generate_temporary_filename
 
 
@@ -92,19 +92,3 @@ class TestTranslation(unittest.TestCase):
             utils.check_url('https://www.no-google.de', raise_error=True, print_warning=False)
         with self.assertRaises(ValueError):
             utils.check_url('https://www.no-google.de', raise_error=True, print_warning=True)
-
-    def test_xml(self):
-        regs = StandardNameTable.get_registered()
-        piv_snt = StandardNameTable.load_registered('piv-v1')
-        xml_filename = utils.dict2xml(generate_temporary_filename(suffix='.xml'),
-                                      name=piv_snt.name,
-                                      dictionary=piv_snt.standard_names,
-                                      versionname=piv_snt.versionname)
-        data, meta = utils.xmlsnt2dict(xml_filename=xml_filename)
-        self.assertEqual(meta['name'], piv_snt.name, )
-        self.assertEqual(meta['versionname'], piv_snt.versionname)
-
-        html_filename = utils.xml_to_html_table_view(xml_filename=xml_filename,
-                                                     html_filename=generate_temporary_filename(suffix='.html'))
-        self.assertTrue(html_filename.exists())
-        self.assertTrue(html_filename.stat().st_size > 0)
