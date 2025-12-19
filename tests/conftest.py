74eea1afa8d912854890a17cfd66befab36eba08
MatthiasProbst <matth.probst@gmail.com>
2025-12-19 06:42:48 +0100

    sleep_after

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,13 @@ from pathlib import Path
 def _force_cache_dir_for_tests():
     # Keep it stable across all tests in the run
     os.environ.setdefault("MYPACKAGE_CACHE_DIR", str(Path(".cache/zenodo")))
+    
+    
+@pytest.fixture()
+def sleep_after():
+    """Fixture that causes a delay after executing a test.
+    Prevents spamming external providers when used, in case of rate limits.
+    """
+    import time
+    yield
+    time.sleep(0.5)
